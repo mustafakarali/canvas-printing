5cfacc5a6da25826ab7ad10d968cf339f0f0edf6
Martin Gingras <martin.p.gingras@gmail.com>
2013-05-28 22:37:06 -0400

    updated php mysql connection

--- gallery.php
+++ gallery.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <?php
-$link = new mysqli("localhost", "root", "", "db_canvas_printing");
-if(!$link) {
-  die("Connection Failed: ". $link->error());
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+
+$link = mysql_connect($server, $username, $password);
+if (!$link) {
+  echo "Can't Link";
+  die('Not connected : ' . mysql_error());
+}      
+
+$db_selected = mysql_select_db($db, $link);
+if (!$db_selected) {
+  echo "Can't connect to database";
+  die ('Can\'t use foo : ' . mysql_error());
 }
-$result = $link->query("SELECT * FROM images");
+
+$result = mysql_query("SELECT * FROM images", $link);
+if (!$result) {
+    throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+}
 
 ?>
 
@@ -36,23 +54,24 @@ $result = $link->query("SELECT * FROM images");
 
       <input type="text" name="selected" id="image_selected" style="visibility:hidden;">
       <?php
-    while($row = $result->fetch_assoc()){
-      echo "<tr>\n";
-      echo "<td><p>". $row['description']. "</p></td>\n";
-      echo "<td> <img src    = 'images/".$row['image_name']."' alt=".$row['image_id']."  width='400' height='200'></td>\n";
+      
+      while($row = mysql_fetch_array($result)){
+        echo "<tr>\n";
+        echo "<td><p>". $row['description']. "</p></td>\n";
+        echo "<td> <img src    = 'images/".$row['image_name']."' alt=".$row['image_id']."  width='400' height='200'></td>\n";
 
-      //  echo "<td><input type='submit' value='Order &rarr;' = name='order_id'></td>";
-      echo "<td><input type='submit' name='". $row['image_name']."' onClick='updateSelected(".$row['image_id'].");' value='Order This Image &rarr;'></td>\n";
-      echo "</tr>";
-    }
-    $result->close();
-    $link->close();
-    ?>
-  </form>
+        //  echo "<td><input type='submit' value='Order &rarr;' = name='order_id'></td>";
+        echo "<td><input type='submit' name='". $row['image_name']."' onClick='updateSelected(".$row['image_id'].");' value='Order This Image &rarr;'></td>\n";
+        echo "</tr>";
+      }
+      $result->close();
+      $link->close();
+      ?>
+    </form>
 
 
 
-</table>
+  </table>
 
 
 </body>
