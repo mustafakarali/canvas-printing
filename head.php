4eea6cbdaa6c50c3fa9da1222fdc9ace4bdd1744
Martin Gingras <martin.p.gingras@gmail.com>
2013-05-31 08:14:56 -0400

    Added Font-Awesome support

--- head.php
+++ head.php
@@ -7,6 +7,7 @@
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Raleway:400,700,200,300,900,800" rel="stylesheet" type="text/css">
 	<title><?php echo "Canvas Printing - $title" ?></title>
 	<link rel="stylesheet" href="css/master.css" type="text/css" media="screen" title="no title" charset="utf-8">
+  <link rel="stylesheet" href="css/font-awesome.css">
 	<script type="text/javascript" src="scripts/javaScripts.js"></script>
 </head>
 <body>
