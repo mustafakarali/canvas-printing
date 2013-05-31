441d53cefc13cd5969bfed07c596e1bb888fc098
Martin Gingras <martin.p.gingras@gmail.com>
2013-05-31 08:15:36 -0400

    migrated index.php to new style

--- index.php
+++ index.php
@@ -2,23 +2,55 @@
 $title = "Home";
 include("head.php");
 ?>
-  <h1>
-    Welcome To Canvas Printing by Martin Gingras
-  </h1>
-  <h2>
-    &nbsp;&nbsp;We have many options for your printing preferences:
-  </h2>
-  <ul>
-    <li>Choose an existing image from our gallery
-    </li>
-    <li>Upload an image of your choosing
-    </li>
-    <li>Pick between multiple printing styles
-    </li>
-    <li>Crop and edit your image online
-    </li>
-  </ul>
-  <!-- <h2>Feel free to browse our Gallery....</h2>
-  <a href="main/gallery.php">Gallery</a> -->
-</body>
-</html>
+<div id="page">
+	<div class="content"> <span>Welcome To Canvas Printing by Martin Gingras</span>
+		<h2>Your #1 Stop for Canvas Printing</h2>
+		<p>We have tons of options to fulfill all of for your printing preferences:</p>
+		<a href="canvas.php" class="button-style button-style-alt">Canvas Types</a> </div>
+</div>
+<div id="featured-wrapper">
+	<div id="box1">
+		<h2>Other Sites<br />
+			<span>by Martin Gingras</span></h2>
+		<p>Check out some of these other sites created by Martin.</p>
+		<ul class="style4">
+			<li class="first"><a href="http://www.synonymizer.herokuapp.com">Synonymizer Web App</a></li>
+      <!-- <li><a href="#">Metus aliquam pellentesque</a></li> -->
+      <!-- <li><a href="#">Proin gravida orci porttitor</a></li> -->
+		</ul>
+		<a href="http://www.magingras.com/" class="button-style">magingras.com</a> </div>
+	<div id="box2">
+		<h2>Upcoming<br />
+			<span>Important events coming to mgprinting</span></h2>
+		<ul class="style3">
+			<li class="first">
+				<p class="date"><a href="#">June<b>15</b></a></p>
+				<h3>Revelation of a new artist</h3>
+				<p><a href="#">Martin Gingras will be sharing his extensive photo collection based on the modeling of Sierra</a></p>
+			</li>
+			<li>
+				<p class="date"><a href="#">June<b>23</b></a></p>
+				<h3>New Collection</h3>
+				<p><a href="#">New collection of landscape pictures to be brought in</a></p>
+			</li>
+			<li>
+				<p class="date"><a href="#">July<b>1</b></a></p>
+				<h3>50% off purchases</h3>
+				<p><a href="#">Our Summer Sale will take place this week, place an order and save up to 50%s</a></p>
+			</li>
+			<li>
+		</ul>
+	</div>
+	<div id="box3">
+		<h2>Options<br />
+			<span>See our many options</span></h2>
+		<ul class="style4">
+			<li class="first"><a href="#">Consectetuer adipiscing elit</a></li>
+			<li><a href="#">Choose an existing image from our gallery</a></li>
+			<li><a href="#">Upload an image of your choosing</a></li>
+			<li><a href="#">Pick between multiple printing styles</a></li>
+			<li><a href="#">Crop and edit your image online</a></li>
+		</ul>
+	</div>
+</div>
+<?php include("footer.php"); ?>
