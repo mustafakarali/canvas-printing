4d865fe77e2c417c0bb00731325f32eeddd59b94
Martin Gingras <martin.p.gingras@gmail.com>
2013-05-31 08:15:51 -0400

    Moved footer to new file to make site more modular

---
+++ footer.php
@@ -0,0 +1,30 @@
+	<div id="footer-content" class="container">
+		<div id="fbox1">
+			<h2>Recent Updates</h2>
+			<ul class="style1">
+				<li class="first"><a href="#">The collection of ... was added</a></li>
+				<li><a href="#">We've brought in some photographs from young aspiring artists</a></li>
+				<li><a href="#">Art weekend in New York was a blast, mgprinting was there in full force</a></li>
+			</ul>
+		</div>
+		<div id="fbox2">
+			<h2>About</h2>
+			<p>This is <strong>mgprinting</strong>, a proof of concept website mocked up by Martin Gingras, using PHP, HTML, JavaScript, and CSS. To see more about Martin visit <a href="http://www.magingras.com/" rel="nofollow">magingras.com</a>.</p>
+			<a href="http://www.magingras.com" class="button-style">magingras.com</a> </div>
+		<div id="fbox3">
+			<h2>Contact</h2>
+      <p>Feel free to reach out to me at <a href="mailto:martin@magingras.com?Subject=Canvas%20Printing">martin@magingras.com</a>.</p>
+			<ul class="style2">
+				<li><a href="http://github.com/mgingras/"><i class="icon-github icon-2x"></i></a></li>
+				<li><a href="http://www.linkedin.com/in/martinpgingras"><i class="icon-linkedin-sign icon-2x"></i></a></li>
+				<li><a href="https://twitter.com/martin_gingras"><i class="icon-twitter-sign icon-2x"></i></a></li>
+				<li><a href="mailto:martin@magingras.com?Subject=Your%20Website" title="Email Me!"><i class="icon-envelope-alt icon-2x"></i></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<p>Copyright (c) 2013 mgprinting.herokuapp.com. All rights reserved. Design by <strong>Martin Gingras from </strong><a href="http://www.magingras.com/" rel="nofollow">magingras.com</a>.</p>
+</div>
+</body>
+</html>
