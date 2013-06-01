ccd012e7caac6e6388fb23e31d3a91325dfc8765
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-01 10:27:01 -0400

    Migrated to new CSS

--- invoice.php
+++ invoice.php
@@ -21,7 +21,7 @@ if (!$db_selected) {
 
 $result = mysql_query("SELECT * FROM images", $link);
 if (!$result) {
-    throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+  throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
 }
 
 $image_selected = $_POST["image_selected"];
@@ -43,18 +43,35 @@ $client = mysql_fetch_array($result);
 $title = "Orders";
 include("head.php");
 ?>
-<div style="text-align: center;">
-<h3>Thank you <?php echo $client['first_name'];?> for Ordering a Canvas Print <br> from Martin Gingras Canvas Printing</h3>
-<p>imageID#: <strong><?php echo "$image_selected";?> </strong></p>
-<p>--------------</p>
-<p>Cost: &nbsp;&nbsp;&nbsp;<strong><?php echo "$cost";?>$</strong></p>
-<p>Shipping:&nbsp; <strong>10$</strong></p>
-<p>Tax: &nbsp;&nbsp;&nbsp;<strong><?php echo "$tax";?>$</strong></p>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________</p>
-<p><strong>Total: <?php echo "$totalCost";?>$</strong></p>
+<div class="content"> <span>Learn about our two different types of canvases</span>
+  <h2>Canvas Types</h2>
+  <!-- <a href="mgprinting.php" class="button-style button-style-alt">What we do...</a> -->
 </div>
-</body>
-<?php 
-mysql_close($link);
-?>
-</html>
+</div> <!-- content -->
+<div id="featured-wrapper">
+<div id="box1">
+  <div style="text-align: center;">
+    <h3>Thank you <?php echo $client['first_name'];?> for Ordering a Canvas Print <br> from Martin Gingras Canvas Printing</h3>
+    <p>imageID#: <strong><?php echo "$image_selected";?> </strong></p>
+    <p>--------------</p>
+    <p>Cost: &nbsp;&nbsp;&nbsp;<strong><?php echo "$cost";?>$</strong></p>
+    <p>Shipping:&nbsp; <strong>10$</strong></p>
+    <p>Tax: &nbsp;&nbsp;&nbsp;<strong><?php echo "$tax";?>$</strong></p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;________</p>
+    <p><strong>Total: <?php echo "$totalCost";?>$</strong></p>
+  </div> <!-- box1 -->
+  <!--div id="box2">
+  <h2 style="text-align: center;padding-top: 150px;">VS</h2>
+  </div> <!-- box2 -->
+  <!--div id="box3">
+    <h3>Gallery Wrap</h3>
+    <table class       = "image">
+    <tr><td><img src = "images/site/gallery.jpg" alt="Gallery Wrap"  width="400" height="200"/></td></tr>
+    <tr><td class    = "caption">A technique for finishing canvas prints where the artist stretches the canvas so that it wraps around the sides The sides are then prepared and primed in the same manner as the face and tend to continue the image appearing on the face.  The term gallery wrap refers to an image that appears on the sides of the frame as well as the front.</td></tr>
+    </table>
+    </div> <!-- box3 -->
+  </div> <!-- feature wrapper -->
+  <?php
+  include("footer.php");
+  mysql_close($link);
+  ?>
