dc67848335dc488e5da6984f982c88bceb03e7c3
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-01 10:27:10 -0400

    Got rid of old menu bar

--- menu_bar.php
+++ menu_bar.php
@@ -1,52 +1,17 @@
-<!-- <div style="text-align: center;">
-  <div style="text-align: left; margin: 1em auto; width: 50%;">
-    <table border="1">
-      <tr>
-        <td>
-          <img src="images/site/logo.gif" alt="logo">
-        </td>
-        <td>
-          <a href="index.php">Home</a>
-        </td>
-        <td>
-          <a href="gallery.php">Gallery</a>
-        </td>
-        <td>
-          <a href="upload.php">Upload</a>
-        </td>
-        <td>
-          <a href="canvas.php">Canvas Types</a>
-        </td>
-        <td>
-          <a href="prices.php">Prices</a>
-        </td>
-        <td>
-          <a href="about.php">About Us</a>
-        </td>
-        <td>
-          <a href="orders.php">Orders</a>
-        </td>
-      </tr>
-    </table>
+<div id="header">
+  <div id="logo">
+    <h1><a href="#"><span>mg</span>printing</a></h1>
+    <p>Gallery and Printing Services to fullfuill all your canvas printing desires</p>
+  </div>
+  <div id="menu">
+    <ul>
+      <!-- Set class to current_page_item to hightlight -->
+      <li class="first"><a href="index.php" accesskey="1" title="">Homepage</a></li>
+      <li><a href="gallery.php" accesskey="2" title="">Gallery</a></li>
+      <li><a href="upload.php" accesskey="3" title="">Upload</a></li>
+      <li><a href="canvas.php" accesskey="4" title="">Canvas Types</a></li>
+      <li><a href="about.php" accesskey="5" title="">About Us</a></li>
+      <li><a href="orders.php" accesskey="6" title="">Orders</a></li>
+    </ul>
   </div>
 </div>
-
-</body>
-</html> -->
-
-  <div id="header">
-    <div id="logo">
-      <h1><a href="#"><span>mg</span>printing</a></h1>
-      <p>Gallery and Printing Services to fullfuill all your canvas printing desires</p>
-    </div>
-    <div id="menu">
-      <ul>
-        <li class="first current_page_item"><a href="index.php" accesskey="1" title="">Homepage</a></li>
-        <li><a href="gallery.php" accesskey="2" title="">Gallery</a></li>
-        <li><a href="upload.php" accesskey="3" title="">Upload</a></li>
-        <li><a href="canvas.php" accesskey="4" title="">Canvas Types</a></li>
-        <li><a href="about.php" accesskey="5" title="">About Us</a></li>
-        <li><a href="orders.php" accesskey="6" title="">Orders</a></li>
-      </ul>
-    </div>
-  </div>
