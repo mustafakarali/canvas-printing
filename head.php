2c5f41ab9ddf728eeda1f7752d7feb1445c764b9
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-01 10:26:46 -0400

    updated css ref and moved page div to head

--- head.php
+++ head.php
@@ -6,10 +6,11 @@
 	<meta name="description" content="" />
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Raleway:400,700,200,300,900,800" rel="stylesheet" type="text/css">
 	<title><?php echo "Canvas Printing - $title" ?></title>
-	<link rel="stylesheet" href="css/master.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="css/master.css" type="text/css" media="all" charset="utf-8">
   <link rel="stylesheet" href="css/font-awesome.css">
 	<script type="text/javascript" src="scripts/javaScripts.js"></script>
 </head>
 <body>
 <div id="wrapper" class="container">
 	<?php include("menu_bar.php"); ?>
+    <div id="page">
