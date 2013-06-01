3283d158086735607ef6ae67521e99c771cc038c
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-01 10:25:13 -0400

    Migrated canvas.php to new CSS

--- canvas.php
+++ canvas.php
@@ -1,24 +1,29 @@
 <?php
-$title = "Canvas Types";
+$title = "About";
 include("head.php");
 ?>
-<h1>Canvas Types:</h1>
-<h2>&nbsp;&nbsp;&nbsp;&nbsp;Learn about our two different types of canvases</h2>
-<h3>Museum Wrap</h3>
-<p>
-  A finishing technique for artwork mounted on stretchers. The fabric is mounted onto stretcher bars with no visible staples on the edge of the frame. Edges are painted dark and no framing is necessary.
-</p>
-<table class       = "image">
-  <tr><td><img src = "images/site/museum.jpg" alt="Museum Wrap"  width="400" height="200"/></td></tr>
-  <tr><td class    = "caption">Museum Wrap</td></tr>
-</table>
-<h3>Gallery Wrap</h3>
-<p>
-  A separate technique for finishing canvas prints. Gallery wrap is a method of stretching an artist's canvas so that the canvas wraps around the sides. The sides of the canvas are prepared and primed in the same manner as the face and tend to continue the image appearing on the face.  the term gallery wrap refers to an image that appears on the sides of the frame as well as the front. 
-</p>
-<table class       = "image">
-  <tr><td><img src = "images/site/gallery.jpg" alt="Gallery Wrap"  width="400" height="200"/></td></tr>
-  <tr><td class    = "caption">Gallery Wrap</td></tr>
-</table>
-</body>
-</html>
+<div class="content"> <span>Learn about our two different types of canvases</span>
+  <h2>Canvas Types</h2>
+  <!-- <a href="mgprinting.php" class="button-style button-style-alt">What we do...</a> -->
+</div>
+</div> <!-- content -->
+<div id="featured-wrapper">
+<div id="box1">
+  <h3>Museum Wrap</h3>
+  <table class       = "image">
+    <tr><td><img src = "images/site/museum.jpg" alt="Museum Wrap"  width="400" height="200"/></td></tr>
+    <tr><td class    = "caption">A finishing technique for artwork mounted on stretchers. The fabric is mounted onto stretcher bars with no visible staples on the edge of the frame. Edges are painted dark and no framing is necessary.</td></tr>
+  </table>
+</div> <!-- box1 -->
+<div id="box2">
+  <h2 style="text-align: center;padding-top: 150px;">VS</h2>
+</div> <!-- box2 -->
+<div id="box3">
+  <h3>Gallery Wrap</h3>
+  <table class       = "image">
+    <tr><td><img src = "images/site/gallery.jpg" alt="Gallery Wrap"  width="400" height="200"/></td></tr>
+    <tr><td class    = "caption">A technique for finishing canvas prints where the artist stretches the canvas so that it wraps around the sides The sides are then prepared and primed in the same manner as the face and tend to continue the image appearing on the face.  The term gallery wrap refers to an image that appears on the sides of the frame as well as the front.</td></tr>
+  </table>
+</div> <!-- box3 -->
+</div> <!-- feature wrapper -->
+<?php include("footer.php"); ?>
