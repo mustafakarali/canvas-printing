bb43a4af39f992ad2001bae60e84e65c93f9f551
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-02 11:13:52 -0400

    updated login

--- login.php
+++ login.php
@@ -1,26 +1,25 @@
 <?php
 
-// $link = new mysqli("localhost", "root", "", "db_canvas_printing");
-// if(!$link) {
-  //   die("Connection Failed: ". $link->error());
-  // }
-
 $selected_image = $_POST['selected'];
 
 $title = "Login";
 include("head.php");
 
 ?>
-<h1>Login to Canvas Printing</h1>
-<form action="place_order.php" method="post" id="myForm">
-  <?php
-  echo "<input type='text' name='selected' value=".$selected_image." style='visibility:hidden;'>";
-  ?>
-
-  <p>E-mail:&nbsp;&nbsp; <input type="text" name="username" value="" placeholder="E-mail" id="username"></p>
-  <p>Password: <input type="password" name="password" placeholder="Password" value="" id="password"></p>
-  <p>Don't have an account?<br><a href="signup.php">sign up for an account now...</a></p>
-  <p><input type="button" onclick="login();" value="Log In &rarr;"></p>
-</form>
-</body>
-</html>
+<div class="content"> <span>Log in to proccess your order</span>
+  <h2>Login to Canvas Printing</h2>
+  <a href="mgprinting.php" class="button-style button-style-alt">What we do...</a>
+</div> <!-- content -->
+</div> <!-- page -->
+<div id="featured-wrapper">
+<div id="fullbox">
+  <form action="place_order.php" method="post" id="myForm">
+    <?php    echo "<input type='text' name='selected' value=".$selected_image." style='visibility:hidden;'>";    ?>
+    <p>E-mail:&nbsp;&nbsp; <input type="text" name="username" value="" placeholder="E-mail" id="username"></p>
+    <p>Password: <input type="password" name="password" placeholder="Password" value="" id="password"></p>
+    <p>Don't have an account?<br><a href="signup.php">sign up for an account now...</a></p>
+    <p><input type="button" onclick="login();" value="Log In &rarr;"></p>
+  </form>
+</div> <!-- fullbox -->
+</div> <!-- feature wrapper -->
+<?php include("footer.php"); ?>
