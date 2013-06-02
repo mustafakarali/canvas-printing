e27d96d717b53ef2f5dcf98baa11f9ce22557bb8
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-02 08:23:04 -0400

    updated upload to new css style

--- upload.php
+++ upload.php
@@ -2,22 +2,23 @@
 $title = "Upload";
 include("head.php");
 ?>
-  <h1>Upload:</h1>
-  <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Your own Images for printing</h2>
+<div class="content"> <span>Upload your our own images for printing</span>
+  <h2>Upload</h2>
+</div>
+</div> <!-- content -->
+<div id="featured-wrapper">
+<div id="box1">
   <form action = "upload_submit.php" method="post"  enctype = "multipart/form-data">
     Pick a image from your hard drive to upload:<br/><br/>
     <input type= "file" id="image_upload" name="image"/><br/><br/>
     <p>Description of Image:</p>
-    <div  style                         = "max-height:400px;max-width:400px;overflow:auto;">
-      <textarea rows                    = "5" cols="52" placeholder="Enter Description of Image here"  name="description" required="required"></textarea>
+    <div style= "max-height:400px;max-width:400px;overflow:auto;">
+      <textarea rows = "5" cols="52" placeholder="Enter Description of Image here"  name="description" required="required"></textarea>
     </div>
     <p>
-      <input type                     = "submit" value="Upload to Gallery&rarr;"/>
+      <input type= "submit" value="Upload to Gallery&rarr;"/>
     </p>
   </form>
-
-</body>
-</html>
-
-</body>
-</html>
+</div> <!-- box1 -->
+</div> <!-- feature wrapper -->
+<?php include("footer.php"); ?>
