798eab7bce9e5762b10aaf77a7940b3d5ab65e9c
Martin Gingras <martin.p.gingras@gmail.com>
2013-06-02 13:23:50 -0400

    new upload method

--- upload_submit.php
+++ upload_submit.php
@@ -10,18 +10,70 @@ include("head.php");
 <div id="featured-wrapper">
 <div id="fullbox">
   <?php
-  if ($_FILES["file"]["error"] > 0)
+  
+  $description = $_POST['description'];
+  
+  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+
+  $link = mysql_connect($server, $username, $password);
+  if (!$link) {
+    echo "Can't Link";
+    die('Not connected : ' . mysql_error());
+  }      
+
+  $db_selected = mysql_select_db($db, $link);
+  if (!$db_selected) {
+    echo "Can't connect to database";
+    die('Can\'t connect to $db : ' . mysql_error());
+  }
+  
+  $allowedExts = array("gif", "jpeg", "jpg", "png");
+  $extension = end(explode(".", $_FILES["file"]["name"]));
+  if ((($_FILES["file"]["type"] == "image/gif")
+  || ($_FILES["file"]["type"] == "image/jpeg")
+  || ($_FILES["file"]["type"] == "image/jpg")
+  || ($_FILES["file"]["type"] == "image/pjpeg")
+  || ($_FILES["file"]["type"] == "image/x-png")
+  || ($_FILES["file"]["type"] == "image/png"))
+  && ($_FILES["file"]["size"] < 20000)
+    && in_array($extension, $allowedExts))
+  {
+    if ($_FILES["file"]["error"] > 0)
     {
-    echo "Error: " . $_FILES["file"]["error"] . "<br>";
+      echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     }
+    else
+    {
+      echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+      echo "Type: " . $_FILES["file"]["type"] . "<br>";
+      echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+      echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+      if (file_exists("images/" . $_FILES["file"]["name"]))
+      {
+        echo $_FILES["file"]["name"] . " already exists. ";
+      }
+      else
+      {
+        move_uploaded_file($_FILES["file"]["tmp_name"],
+        "upload/" . $_FILES["file"]["name"]);
+        echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
   else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Stored in: " . $_FILES["file"]["tmp_name"];
-    }
+  {
+    echo "Invalid file";
+  }
+  $sql = "INSERT INTO images(image_name, description) values ($_FILES['file']['name'], '$description')";
+  $result = mysql_query($sql, $link);
+  mysql_close($link);
   ?>
 </div> <!-- fullbox -->
-    </div> <!-- feature wrapper -->
-    <?php include("footer.php"); ?>
+</div> <!-- feature wrapper -->
+<?php include("footer.php"); ?>
